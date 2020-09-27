0c950b17206ed2a51b8f19618038aa9874a893d3
vaishak <vaishak.k@zohocorp.com>
2020-09-27 18:46:52 +0530

    added the tweetid

--- model/post_model.go
+++ model/post_model.go
@@ -1,6 +1,7 @@
 package model
 
 type Post struct {
+	TweetID     int    `json:"tweetid"`
 	TweetText   string `json:"tweet_text"`
 	PublishTime string `json:"time"`
 	UserName    string `json:"username"`
